b84b060e99a6a00afc3d5a9cc110a7dc4b4fa022
js-ha <jshaha0911@gmail.com>
2023-06-18 23:57:12 +0900

    🎨Design:  탭 클릭시 글씨 색 변경 적용 테스트

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,13 +1,19 @@
 import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
+import { useLocation } from 'react-router-dom';
 
 function NavTabsExample() {
+  const location = useLocation();
+  const urlName = location.pathname;
+
   return (
     <Card style={{ width: '100%' }}>
       <Card.Header>
         <Nav variant='tabs'>
           <Nav.Item>
-            <Nav.Link href='/vegetable'>채소류</Nav.Link>
+            <Nav.Link href='/vegetable' className={urlName === '/vegetable' ? 'active' : ''}>
+              채소류
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link href='/meat_fish'>고기/생선류</Nav.Link>
